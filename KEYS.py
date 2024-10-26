39652abc3af79cd177a3d1e7258bdcb1b6bcc508
KHJKHJKKKHHHJJJ <137582646+KHJKHJKKKHHHJJJ@users.noreply.github.com>
2024-10-26 22:32:12 +0900

    Removed keys

--- KEYS.py
+++ KEYS.py
@@ -1,15 +1,9 @@
 # Gemini api
-Gemini = "AIzaSyBZeuqYrfyBA2hTId9-LxLorRHlT_Wan6A"
+Gemini = "YOUR API KEY"
 
 # Coinone API
-Access = "0c1f325b-695f-4357-a5d4-d0bfb2e096f5"
-Secret = bytes('7d456567-4396-40e6-9ea0-8c4e5a74035e', 'utf-8')
-
-# NEWS API
-news = "764b97e66b214508b4868a730d848f13"
-
-# Whats App
-wa = "GjJvnB1RmlQkfVFs54dNMBpxhZSEhFJWRSlYagHu32e77223"
+Access = "YOUR API KEY"
+Secret = bytes('YOUR API KEY', 'utf-8')
 
 # Telegram API
-tel = "7764392156:AAFzEfs-i9y9978fANwjvkCqGkbzUHhZqaw"
+tel = "YOUR API KEY"
