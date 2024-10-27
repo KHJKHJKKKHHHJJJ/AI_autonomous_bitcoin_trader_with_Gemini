96b8fb703a0e877419c274bfe5d481b0f40a4c30
KHJKHJKKKHHHJJJ <137582646+KHJKHJKKKHHHJJJ@users.noreply.github.com>
2024-10-27 11:55:36 +0900

    Update Prud_AI.py

--- Prud_AI.py
+++ Prud_AI.py
@@ -128,8 +128,6 @@ def write_fear_greed():
     '''Records Fear_Greed Index of its date.
     It's void function'''
     headers = {
-        "ACCEPT" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        "USER-AGENT" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"
     }
 
     url = "https://www.blockstreet.co.kr/fear-greed"
