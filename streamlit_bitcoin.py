9ebca26699d3de03de7694f7ebfc1a430d42aa58
KHJKHJKKKHHHJJJ <137582646+KHJKHJKKKHHHJJJ@users.noreply.github.com>
2024-11-05 22:41:06 +0900

    prudence index error fix

--- streamlit_bitcoin.py
+++ streamlit_bitcoin.py
@@ -15,7 +15,12 @@ st.header("Welcome To Gemini Bitcoin Assistant!")
 
 status = st.container(border = True)
 col1, col2 = status.columns(2)
-tfg, yfg = fgtable.iloc[-1:-3:-1, 1]
+if fgtable.count() > 2:
+  tfg, yfg = fgtable.iloc[-1:-3:-1, 1]
+else:
+  tfg = fgtable.iloc[-1, 1]
+  yfg = tfg
+  
 col1.metric("Today's Fear Greed Index", f"{tfg}", f"{tfg - yfg}")
 tpi, ypi = prudence_table.iloc[-1:-3:-1, 1]
 col2.metric("Today's Prudence Index", f"{tpi}", f"{tpi - ypi}")
