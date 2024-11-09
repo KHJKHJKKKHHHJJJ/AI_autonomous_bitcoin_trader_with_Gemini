25cb7e4de934152b1950fc75b321a84464afc3fd
KHJKHJKKKHHHJJJ <137582646+KHJKHJKKKHHHJJJ@users.noreply.github.com>
2024-11-09 12:59:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,6 @@ while u_input not in ['Y', 'y', 'Yes', 'yes', 'YES']:
         dbop = sql.connect("./Record.db")
         dbcs = dbop.cursor()
         stamp = list(dbcs.execute("SELECT DATE FROM PRUDENCERECORD;"))
-        print(stamp)
         # if ((datetime.datetime.now() - timestamp) > datetime.timedelta(hours=23) or len(stamp) == 0 or stamp[-1][0] != str(timestamp)[:10]) and stamp[-1][0] != str(datetime.datetime.now())[:10]:
         # Only for 11-09
         if False:
@@ -48,7 +47,7 @@ while u_input not in ['Y', 'y', 'Yes', 'yes', 'YES']:
             pru_instruction = './Prudence Gemini Instruction.md'
             pru_chat_session = Prud_AI.gen_pru_model(get_instructions(pru_instruction))
             b = Prud_AI.gem_pru_sug(pru_chat_session)
-            bit_AI.model_usage(b)
+
     
             try:
                 with sql.connect("./Record.db") as dbop:
@@ -79,7 +78,6 @@ while u_input not in ['Y', 'y', 'Yes', 'yes', 'YES']:
         time.sleep(30)
         try:
             decision = bit_AI.gem_sug(bit_chat_session, bit_AI.get_today_prudence())
-            bit_AI.model_usage(decision)
             # for debugging
             # decision = dict()
             # decision['decision'] = 'sell'
